aaa443df60e46e1e755e039f0973522cfdea4ca8
JasperZeroes <Jasperobed@gmail.com>
2022-03-14 08:03:48 -0700

    added a file mam.c

---
+++ mam.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+/* main - prints hello world 
+ * on to standard output
+ *
+ * Returns : Always 0 (successful)
+ */
+
+int main(void)
+{
+	printf("Hello, world!\n");
+	return (0);
+}
