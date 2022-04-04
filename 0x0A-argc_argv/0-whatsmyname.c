70db8561fabf8fb3faeca638fac13afc23f4c08d
JasperZeroes <Jasperobed@gmail.com>
2022-04-04 01:29:49 -0700

    added a file that that prints out its pragram name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,20 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-/** main- function that takes any amount
- *  of argument and prints it to standard output
- *  exit :(EXIT_SUCCESSFUL)
- */
+#include<stdio.h>
+#include<stdlib.h>
+/*
+* main - function prints out the name of
+* its program name to standard output
+* Returns : EXIT_SUCCESS
+*/
 int main(int argc, char *argv[])
 {
-    int count;
-    if (argc > 0)
-    {
-    	for (count = 0; count < argc; count++)
-        {
-        	printf("%s\n", argv[count]);
-        }
-    }
-    exit (EXIT_SUCCESS);
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	exit(EXIT_SUCCESS);
 }
-
