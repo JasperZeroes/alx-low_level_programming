e024cf251178b0075b0aac6f83f1874704a9d3a6
JasperZeroes <Jasperobed@gmail.com>
2022-04-15 08:23:07 -0700

    added my own method of solving the same problem

--- 0x10-variadic_functions/1-print_numbers_my_version.c
+++ 0x10-variadic_functions/1-print_numbers_my_version.c
@@ -8,21 +8,24 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	va_list list;
+	int nums;
+	va_list args;
 
-	va_start(list, n);
+	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		if (!separator)
-			printf("%d", va_arg(list, int));
-		else if (separator && i == 0)
-			printf("%d", va_arg(list, int));
-		else
-			printf("%s%d", separator, va_arg(list, int));
+		nums = va_arg(args, int);
+
+		if (separator == NULL)
+			printf("%d ", nums);
+
+		printf("%d", nums);
+
+		if (i != (n - 1))
+			printf("%s", separator);
 	}
 
-	va_end(list);
-
+	va_end(args);
 	printf("\n");
 }
